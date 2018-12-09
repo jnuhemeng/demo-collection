4aa06e18b7e216303e87ef72e8bd845b69f31c19
secfbth <secfbth@lemon.lemon.net>
2018-12-09 11:55:15 +0800

    add netty demo

---
+++ netty/user-guide-client/src/main/java/my/netty/demo/time/TimeDecoder.java
@@ -0,0 +1,28 @@
+package my.netty.demo.time;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class TimeDecoder extends ByteToMessageDecoder { // (1)
+    //ByteToMessageDecoder calls the decode() method with an internally maintained cumulative buffer whenever new data is received.
+    //Please remember that you don't need to decode multiple messages. ByteToMessageDecoder will keep calling the decode() method until it adds nothing to out.
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
+        if (in.readableBytes() < 4) {
+            return; // (3)
+        }
+
+        out.add(in.readBytes(4)); // (4)
+    }
+}
+/*
+public class TimeDecoder extends ReplayingDecoder<Void> {
+    @Override
+    protected void decode(
+            ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+        out.add(in.readBytes(4));
+    }
+}*/
